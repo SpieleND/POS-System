9ee143d10974758809aecf6490a34b9da839484c
Matthias Gabel <matthias-gabel@hotmail.de>
2025-04-13 20:39:04 +0200

    update seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,43 +2,88 @@ import { PrismaClient, Prisma } from '../src/app/generated/prisma'
 
 const prisma = new PrismaClient()
 
-const userData: Prisma.UserCreateInput[] = [
-  {
-    name: 'Alice',
-    email: 'alice@prisma.io',
-    posts: {
-      create: [
-        {
-          title: 'Join the Prisma Discord',
-          content: 'https://pris.ly/discord',
-          published: true,
-        },
-        {
-          title: 'Prisma on YouTube',
-          content: 'https://pris.ly/youtube',
-        },
-      ],
-    },
-  },
-  {
-    name: 'Bob',
-    email: 'bob@prisma.io',
-    posts: {
-      create: [
-        {
-          title: 'Follow Prisma on Twitter',
-          content: 'https://www.twitter.com/prisma',
-          published: true,
-        },
-      ],
-    },
-  }
-]
-
 export async function main() {
-  for (const u of userData) {
-    await prisma.user.create({ data: u })
-  }
+  
+  // Seed Roles
+  const adminRole = await prisma.role.create({
+    data: {
+      name: 'Admin',
+    },
+  })
+  
+  const userRole = await prisma.role.create({
+    data: {
+      name: 'User',
+    },
+  })
+  
+
+  // Seed Users
+  const adminUser = await prisma.user.create({
+    data: {
+      username: 'admin',
+      rfidKey: '123456',
+      role: { connect: { id: adminRole.id } },
+    },
+  })
+
+  const regularUser = await prisma.user.create({
+    data: {
+      username: 'user1',
+      rfidKey: '654321',
+      role: { connect: { id: userRole.id } },
+    },
+  })
+
+  // Seed Products
+  const product1 = await prisma.product.create({
+    data: {
+      name: 'Product A',
+      buy: 10.0,
+      sell: 15.0,
+    },
+  })
+
+  const product2 = await prisma.product.create({
+    data: {
+      name: 'Product B',
+      buy: 20.0,
+      sell: 30.0,
+    },
+  })
+
+  // Seed Orders
+  await prisma.order.create({
+    data: {
+      user: { connect: { id: adminUser.id } },
+      product: { connect: { id: product1.id } },
+      quantity: 2,
+      paid: true,
+      aproved: true,
+      createdBy: 'system',
+      updatedBy: 'system',
+    },
+  })
+
+  await prisma.order.create({
+    data: {
+      user: { connect: { id: regularUser.id } },
+      product: { connect: { id: product2.id } },
+      quantity: 1,
+      paid: false,
+      aproved: false,
+      createdBy: 'system',
+      updatedBy: 'system',
+    },
+  })
 }
 
 main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
