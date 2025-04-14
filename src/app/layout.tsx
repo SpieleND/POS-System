818c4f7e91d70c2686c9c62fa7c9d741301019ea
Matthias Gabel <matthias-gabel@hotmail.de>
2025-04-14 17:06:08 +0200

    add mui to project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import { ThemeProvider } from "@mui/material";
+import theme from "../theme";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +28,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <AppRouterCacheProvider
+          options={{ key: 'css' }}
+        >
+          <ThemeProvider theme={theme}>
+            {children}
+          </ThemeProvider>
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,18 @@
 import prisma from '@/lib/prisma'
+import { Button } from '@mui/material';
 
 export default async function Home() {
-  const users = (await prisma.user.findMany());
   const products = (await prisma.product.findMany());
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16">
-      <h1 className="text-4xl font-bold mb-8 font-[family-name:var(--font-geist-sans)] text-[#333333]">
-        Superblog
+    <div>
+      <h1>
+        Kassensystem
       </h1>
-      <ol className="list-decimal list-inside font-[family-name:var(--font-geist-sans)]">
-        {users.map((user) => (
-          <li key={user.id} className="mb-2">
-            {user.username}
-          </li>
+        {products.map((product) => (
+            <Button key={product.id}>
+            {product.name}
+            </Button>
         ))}
-      </ol>
     </div>
   );
 }
