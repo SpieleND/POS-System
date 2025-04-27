3ff2714f353a72d8bf75e20b72c0f999b5254412
Matthias Gabel <matthias-gabel@hotmail.de>
2025-04-27 14:15:19 +0200

    add transition effect

--- src/app/ui/OrderItemsSidebar.tsx
+++ src/app/ui/OrderItemsSidebar.tsx
@@ -4,6 +4,7 @@ import { ShoppingCartOutlined } from '@mui/icons-material'
 import {
   Alert,
   Button,
+  Collapse,
   Divider,
   List,
   ListItemButton,
@@ -14,9 +15,10 @@ import {
 } from '@mui/material'
 import { grey } from '@mui/material/colors'
 import { useContext } from 'react'
+import { TransitionGroup } from 'react-transition-group'
+import { Product } from '../generated/prisma'
 import { toEuro } from '../lib/to-euro'
 import { CartOrderItem, OrderContext } from '../products/page'
-import { Product } from '../generated/prisma'
 
 const sumOrderItems = (orderItems: CartOrderItem[], products: Product[]) => {
   return orderItems.reduce((sum, item) => {
@@ -29,36 +31,43 @@ const sumOrderItems = (orderItems: CartOrderItem[], products: Product[]) => {
 }
 
 export default function OrderItemsSidebar() {
-  const { orderItems, products, resetOrderItems, removeItem } = useContext(OrderContext)
+  const { orderItems, products, resetOrderItems, removeItem } =
+    useContext(OrderContext)
 
   return (
     <Stack gap={2} height={'100%'}>
-      <List style={{ flexGrow: 1, alignContent: 'end'}}>
+      <List style={{ flexGrow: 1, alignContent: 'end' }}>
         {orderItems.length > 0 ? (
-          orderItems.map((item, index) => {
-            const product = products.find((product) => product.id === item.productId)
-            if (!product)
-              return (
-                <Alert key={index} severity="error">
-                  Product Id not found.
-                </Alert>
+          <TransitionGroup>
+            {orderItems.map((item, index) => {
+              const product = products.find(
+                (product) => product.id === item.productId,
               )
+              if (!product)
+                return (
+                  <Collapse key={index}>
+                    <Alert severity="error">Product Id not found.</Alert>
+                  </Collapse>
+                )
 
-            return (
-              <ListItemButton key={index} onClick={() => removeItem(product.id)}>
-                <ListItemIcon>
-                  <ShoppingCartOutlined />
-                </ListItemIcon>
-                <ListItemText
-                  primary={product.name}
-                  secondary={toEuro(product.sell)}
-                />
-                <Typography variant="h5" color={grey[500]}>
-                  x{item.amount}
-                </Typography>
-              </ListItemButton>
-            )
-          })
+              return (
+                <Collapse key={index}>
+                  <ListItemButton onClick={() => removeItem(product.id)}>
+                    <ListItemIcon>
+                      <ShoppingCartOutlined />
+                    </ListItemIcon>
+                    <ListItemText
+                      primary={product.name}
+                      secondary={toEuro(product.sell)}
+                    />
+                    <Typography variant="h5" color={grey[500]}>
+                      x{item.amount}
+                    </Typography>
+                  </ListItemButton>
+                </Collapse>
+              )
+            })}
+          </TransitionGroup>
         ) : (
           <Typography color={grey[500]} variant="body2">
             Kein Produkt ausgewählt
@@ -67,13 +76,16 @@ export default function OrderItemsSidebar() {
       </List>
       <Divider />
       <Stack direction={'row'} justifyContent={'space-between'} padding={2}>
-
-      <h3>Summe:</h3>
-      <h3>{toEuro(sumOrderItems(orderItems, products))}</h3>
+        <Typography variant="h6">Summe:</Typography>
+        <Typography variant="h6">
+          {toEuro(sumOrderItems(orderItems, products))}
+        </Typography>
       </Stack>
-      <Stack direction={'row'} justifyContent={'space-between'} >
+      <Stack direction={'row'} justifyContent={'space-between'}>
         <Button variant="contained">Bezahlen</Button>
-        <Button variant="outlined" onClick={() => resetOrderItems()}>Abbruch</Button>
+        <Button variant="outlined" onClick={() => resetOrderItems()}>
+          Abbruch
+        </Button>
       </Stack>
     </Stack>
   )
